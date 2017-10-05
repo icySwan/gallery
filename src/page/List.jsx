bf4bc5ede13823bfd4aa1cdcef5c2fc92b0e08f0
李寻欢 <ian.primary.li@gmail.com>
2017-10-05 18:07:15 +0800

    日本的照片加载完成。。。

--- src/page/List.jsx
+++ src/page/List.jsx
@@ -4,15 +4,55 @@ import Swipe from 'react-swipe';
 
 import '../style/list.less';
 
+const pfx = (window.innerWidth < 800) ? '?x-oss-process=style/percent50' : '';
 class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: getList(this.props.query.data)
+            list: getList(this.props.query.data),
+            lastIndex: 0
+        }
+    }
+    onClick() {
+        return (e)=> {
+            let {lastIndex} = this.state;
+            this.setState({
+                lastIndex: ++lastIndex
+            });
+        }
+    }
+    onShow(target) {
+        return (e)=> {
+            this.setState({
+                target
+            });
+        }
+    }
+    onHide() {
+        return (e)=> {
+            this.setState({
+                target: null
+            });
         }
     }
     render() {
-        return <div className="list">list</div>;
+        const {list, lastIndex, target} = this.state;
+        const listForShow = list.slice(0, 10 * (1 + lastIndex));
+        return <div className="list">
+            {
+                listForShow.map((item, index)=> {
+                    if(item) {
+                        return <img src={item + pfx} onClick={this.onShow(item)} className="cell" key={index} />
+                    }
+                })
+            }
+            {
+                target ? <div className="show" onClick={this.onHide()}>
+                    <img src={target} className="show-img"/>
+                </div>: null
+            }
+            <div className="more" onClick={this.onClick()}>click to load more...</div>
+        </div>;
     }
 }
 
