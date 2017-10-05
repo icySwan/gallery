fc1de2bda0341ba8afde16f6ed00d5fa735bbfb1
李寻欢 <ian.primary.li@gmail.com>
2017-10-05 14:46:06 +0800

    add files

---
+++ src/constents/home.js
@@ -0,0 +1,37 @@
+const urlRoot = '//swan-photo.oss-cn-hangzhou.aliyuncs.com';
+export default {
+    items : [
+        {
+            url: urlRoot + "/6176645488_IMG_3628.JPG",
+            doc: {
+                date: "APRIL 9th, 2017",
+                title: "DANCE OF SAKURA",
+                des: "Tomb-sweeping Day 2017, Annual travel and It's also birthday present for Mother and Pet. We meet the dance of sakura again in Kyoto"
+            }
+        },
+        {
+            url: urlRoot + "/6176645488_IMG_3628.JPG",
+            doc: {
+                date: "APRIL 9th, 2017",
+                title: "DANCE OF SAKURA",
+                des: "Tomb-sweeping Day 2017, Annual travel and It's also birthday present for Mother and Pet. We meet the dance of sakura again in Kyoto"
+            }
+        },
+        {
+            url: urlRoot + "/6176645488_IMG_3628.JPG",
+            doc: {
+                date: "APRIL 9th, 2017",
+                title: "DANCE OF SAKURA",
+                des: "Tomb-sweeping Day 2017, Annual travel and It's also birthday present for Mother and Pet. We meet the dance of sakura again in Kyoto"
+            }
+        },
+        {
+            url: urlRoot + "/6176645488_IMG_3628.JPG",
+            doc: {
+                date: "APRIL 9th, 2017",
+                title: "DANCE OF SAKURA",
+                des: "Tomb-sweeping Day 2017, Annual travel and It's also birthday present for Mother and Pet. We meet the dance of sakura again in Kyoto"
+            }
+        }
+    ]
+}
