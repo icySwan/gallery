77134cbc95bdbea48a631c3bd4b316f4eba2a666
李寻欢 <ian.primary.li@gmail.com>
2017-11-05 15:12:37 +0800

    加了个简单分类

--- src/page/List.jsx
+++ src/page/List.jsx
@@ -9,17 +9,41 @@ const pfx = (window.innerWidth < 800) ? '?x-oss-process=style/percent50' : '';
 class List extends Component {
     constructor(props) {
         super(props);
+        let list = getList(this.props.query.data);
+
+        let categoryLastIndex = {};
+        let selectedCategory = null;
+        if(list.length >=1 && list[0].category) {
+            list.forEach((item, index)=> {
+                if(index === 0) {
+                    selectedCategory = item.category;
+                }
+                categoryLastIndex[item.category] = 0;
+            });
+        }
+        const lastIndex = 0;
+
         this.state = {
-            list: getList(this.props.query.data),
-            lastIndex: 0
+            list,
+            lastIndex,
+            categoryLastIndex,
+            selectedCategory
         }
     }
-    onClick() {
+    onClick(category) {
         return (e)=> {
-            let {lastIndex} = this.state;
-            this.setState({
-                lastIndex: ++lastIndex
-            });
+            if(category) {
+                let {categoryLastIndex} = this.state;
+                categoryLastIndex[category] = 1 + categoryLastIndex[category];
+                this.setState({
+                    categoryLastIndex
+                })
+            } else {
+                let {lastIndex} = this.state;
+                this.setState({
+                    lastIndex: ++lastIndex
+                });
+            }
         }
     }
     onShow(target) {
@@ -36,25 +60,77 @@ class List extends Component {
             });
         }
     }
+    swipeEnd() {
+        return (i, e)=> {
+            const {selectedCategory, list} = this.state;
+            const newCategory = list[i]['category'];
+            if(selectedCategory !== newCategory) {
+                this.setState({
+                    selectedCategory: newCategory
+                });
+            }
+        }
+    }
     render() {
-        const {list, lastIndex, target} = this.state;
-        const listForShow = list.slice(0, 10 * (1 + lastIndex));
-        return <div className="list">
-            <Nav current="gallery" />
-            {
-                listForShow.map((item, index)=> {
-                    if(item) {
-                        return <img src={item + pfx} onClick={this.onShow(item)} className="cell" key={index} />
+        const {list, lastIndex, target, categoryLastIndex, selectedCategory} = this.state;
+        //带分类的
+        if(list.length >=1 && list[0].category) {
+            return <div className="list">
+                <Nav current="gallery" />
+                <div className="category-list">
+                    {
+                        list.map((item, index)=> {
+                            const cls = selectedCategory === item.category ? " selected" : "";
+                            return <div className="cell" key={item.category}><span className={"txt" + cls}>{item.category}</span></div>
+                        })
                     }
-                })
-            }
-            {
-                target ? <div className="show" onClick={this.onHide()}>
-                    <img src={target} className="show-img"/>
-                </div>: null
-            }
-            <div className="more" onClick={this.onClick()}>click to load more...</div>
-        </div>;
+                </div>
+                <Swipe swipeOptions={{
+                    transitionEnd: this.swipeEnd()
+                }}>
+                    {
+                        list.map((item, index)=> {
+                            const category = item.category;
+                            const imgsForShow = item.imgs.slice(0, 10 * (1 + categoryLastIndex[category]));
+                            return <div className="list-content" key={index}>
+                                {
+                                    imgsForShow.map((imgSrc, key)=> {
+                                        if(imgSrc) {
+                                            return <img src={imgSrc + pfx} onClick={this.onShow(imgSrc)} className="cell" key={key} />
+                                        }
+                                    })
+                                }
+                                <div className="more" onClick={this.onClick(category)}>click to load more...</div>
+                            </div>
+                        })
+                    }
+                </Swipe>
+                {
+                    target ? <div className="show" onClick={this.onHide()}>
+                        <img src={target} className="show-img"/>
+                    </div>: null
+                }
+            </div>
+        } else {
+            //普通的
+            const listForShow = list.slice(0, 10 * (1 + lastIndex));
+            return <div className="list">
+                <Nav current="gallery" />
+                {
+                    listForShow.map((item, index)=> {
+                        if(item) {
+                            return <img src={item + pfx} onClick={this.onShow(item)} className="cell" key={index} />
+                        }
+                    })
+                }
+                {
+                    target ? <div className="show" onClick={this.onHide()}>
+                        <img src={target} className="show-img"/>
+                    </div>: null
+                }
+                <div className="more" onClick={this.onClick()}>click to load more...</div>
+            </div>;
+        }
     }
 }
 
